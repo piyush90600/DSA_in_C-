55efe8667bc61b588483ff13fe304b90d9043609
Piyush <piyush90600@gmail.com>
2026-07-08 00:45:32 +0530

    Factorial of n number

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,16 @@
+// factorial of n number
+
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    if (n==0)
+        return 1;
+    return n*fact(n-1);
+
+}
+int main(){
+    int n; cin>>n;
+    int f = fact(n);
+    cout<<f;
+}
