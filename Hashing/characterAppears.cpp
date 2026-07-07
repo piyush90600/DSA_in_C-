542c5cbaefcbf876c06a2a5e2342d2d23cdd60a1
Piyush <piyush90600@gmail.com>
2026-07-08 00:43:26 +0530

    count character of times a character appears in an string using array

---
+++ Hashing/characterAppears.cpp
@@ -0,0 +1,33 @@
+//count character of times a character appears in an string using array
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    int n = s.length();
+
+    //brute force
+    // int count=0;
+    // char c; cin>>c;
+    // for(int i=0;i<n;i++){
+    //     if(s[i]==c) count++;
+    // }
+    // cout<<count;
+
+
+    int hash[26] ={0};
+    for(int i=0;i<n;i++){
+        hash[s[i]-'a'] +=1;
+    }
+    //fetch
+    int q; cin>>q;
+    while(q--){
+        char ch; cin>>ch;
+        cout<<hash[ch-'a']<<" ";
+    }
+
+    return 0;
+
+}
