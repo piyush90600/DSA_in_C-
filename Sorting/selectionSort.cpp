850a4467c276e62fe6806eb6a05163148672821a
Piyush <piyush90600@gmail.com>
2026-07-22 20:23:58 +0530

    Selection Sort

---
+++ Sorting/selectionSort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void selection_sort(int arr[], int n){
+    for(int i=0;i<=n-2;i++){
+        int min=i;
+        for(int j=i;j<=n-1;j++){
+            if(arr[j]<arr[min]){
+                min = j;
+            }
+        }
+        swap(arr[i], arr[min]);
+    }
+}
+
+int main(){
+    int n; cin>>n;
+    int arr[n];
+    for(int i=0;i<=n-1;i++) cin>>arr[i];
+
+    selection_sort(arr, n);
+    for(int i=0;i<=n-1;i++) cout<<arr[i]<<" ";
+
+    return 0;
+}
